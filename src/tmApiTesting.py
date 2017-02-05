164bafc2a3960e505f8e41484bb263bf81f99ef1
root <root@instance-1.c.resell-157718.internal>
2017-02-05 18:28:37 +0000

    Added active-presale, event filter, and presale emaiol formatting updates

--- src/tmApiTesting.py
+++ src/tmApiTesting.py
@@ -6,14 +6,17 @@ time = datetime.datetime.now
 
 discovery_root_url = 'https://app.ticketmaster.com/discovery/v2/'
 
-def getEvents(max_count = 1000):
+def getEvents(max_count = 1000, api_param_dict = {}):
     # DESCRIPTION: Get All Event Jsons. Return a list of event jsons
     # num: max number of events to pull
     page = 0
     events_list = []
-    base_request_url = 'https://app.ticketmaster.com/discovery/v2/events.json?size=20&page={}&apikey=qDPfiZcjOMUmxf8zoUbGvUFJkGqKTkx9'
+    base_request_url = 'https://app.ticketmaster.com/discovery/v2/events.json?size=100&{}&apikey=qDPfiZcjOMUmxf8zoUbGvUFJkGqKTkx9'
+    argument_format_string = "&".join([x+'='+api_param_dict[x] for x in api_param_dict.keys()])
     while True: # Get 20 events at a time until there are no more events left to gather
-        all_events_request = base_request_url.format(page)
+        page_format_string = 'page={}'.format(page)
+        temp_format_string = '&'.join([argument_format_string, page_format_string])
+        all_events_request = base_request_url.format(temp_format_string)
         all_events_json = requests.get(all_events_request).json()
         try:
             [events_list.append(x) for x in all_events_json['_embedded']['events']] # Add event jsons
@@ -30,14 +33,30 @@ def getPriceJsonList(eventJson):
     priceRanges_json_list = eventJson['priceRanges']
     return priceRanges_json_list
 
-def getSalesJson(eventJson, kind = None):
+def getSalesJson(eventJson, kind = None, active = False):
     # DESCRIPTION: Get on-sale information for the given event json, including presale information
 	# kind: if 'presales', return only presale. If 'public', return only public sales
+    # Note: The active argument only works on presales
+    # Note: if kind = 'presales' and active = True, the returned json will only have active presales
+    #       and presales going active in < 2 days
     sale_json = eventJson['sales']
     if kind == 'presales':
-        sale_json = sale_json['presales']
+        sale_json_list = sale_json['presales']
+        if active == True: # Filter for active presales
+            curr_date = time()
+            active_json_list = []
+            for item in sale_json_list: # Remove inactive list items
+                start_date = pd.to_datetime(item[u'startDateTime'])
+                end_date = pd.to_datetime(item[u'endDateTime'])
+                start_date2 = pd.to_datetime(item[u'startDateTime']) - datetime.timedelta(2)
+                if (curr_date > start_date2) and (curr_date < end_date): # Remove presale
+                    active_json_list.append(item)
+            return active_json_list
+        else:
+            return sale_json_list
     elif kind == 'public':
-        sale_json = sale_json['public']
+        sale_json_list = sale_json['public']
+        return sale_json_list
     return sale_json
 
 def getDateJson(eventJson):
@@ -60,18 +79,16 @@ def pretty(d, indent=0):
     # Return a pretty dictionary/json (d) string
     final_string = ''
     for key, value in d.items():
-        final_string += '\n' + ('\t' * indent + str(key))
-        if isinstance(value, dict):
-            final_string += '\n' + pretty(value, indent+1)
-        else:
-            final_string += '\n' + ( '\t' * (indent+1) + str(value.encode('utf-8')))
-    final_string = ('\t' * (indent) + '{') + final_string + ('\n' + ('\t' * (indent)) + '}') # Format like json
+        final_string += '\n' + ('\t' * indent + "{}:{}{}".format(str(key), '\t', str(value)))
     return final_string
 
 def emailPresaleInfo(event_list, address_list):
     # For all evnent-jsons in event_list, email all presale information to each address in 
     # address_list
+    # Note: This method will only pull active presales and has a filter to only report active presales
+    #       and presales going active in under 2 days
     full_presale_info_string = ''
+    success_count = 0
     for event in event_list:
         url = event['url'].encode('utf-8')
         name = event['name'].encode('utf-8')
@@ -83,16 +100,21 @@ def emailPresaleInfo(event_list, address_list):
         venue = event['_embedded']['venues'][0]
         venue_string = ", ".join([venue['name'], venue['city']['name'], venue['state']['stateCode']]).encode('utf-8')
         event_header_string = "\n".join([name,date,venue_string,url])
-        try:
-            presales_json = getSalesJson(event, 'presales')
+        try: # Get active presales and format the email
+            presales_json = getSalesJson(event, 'presales', active = True) # Get active presales
+            if len(presales_json) == 0: # No active presales for this event
+                continue
             pretty_sales = '\n'.join([pretty(x, 1) for x in presales_json]).decode('utf-8')
             presale_info_string = "\n===\n".join([event_header_string, pretty_sales])
             full_presale_info_string += '\n============\n' + presale_info_string.encode('utf-8')
+            success_count += 1
         except Exception, e:
+            # Note: most of these errors are because the event is listed on ticketsnow.com, ticketweb.com
+            #       or livenation.com.... needs to be fixed
             print('ERROR | Could not extract presale info for --> {}'.format(','.join([name,url])))
             continue
     subject = 'Presale Notifications: {}'.format(time())
-    msg = '======================\n==>PRESALE INFO\n==>{}\n==>Total Observations: {}\n======================\n{}'.format(time(), len(event_list), full_presale_info_string)
+    msg = '======================\n==>PRESALE INFO\n==>{}\n==>Successful Observations: {}/{}\n======================\n{}'.format(time(), success_count, len(event_list), full_presale_info_string)
     for address in address_list:
         notifyByEmail(address, subject, msg)
     print('SUCCESS | Sent Presale Email to {} at {}'.format(address_list, time()))
@@ -106,22 +128,18 @@ def emailPresaleInfo(event_list, address_list):
 
 if __name__ == '__main__':
     full_presale_info_string = ''
-    event_list = getEvents()
-    '''
-    for event in events_list:
+    latlong = ','.join(['37.867197', '-122.249933'])
+    #event_list = getEvents(api_param_dict={'countryCode': 'US', 'stateCode': 'CA'}) #California events
+    event_list = getEvents(api_param_dict={'latlong': latlong, 'radius': '50', 'unit': 'miles'})
+    for event in event_list[:2]:
         print '-' * 50
         print('EVENT | {}: {}'.format(event['name'], event['url']))
         priceJson_list = getPriceJsonList(event)
         print('PRICE | {}'.format(priceJson_list))
-        sales_json = getSalesJson(event, 'presales')
-        print('SALE | {}'.format(sales_json))
+        sales_json_list = getSalesJson(event, 'presales', active = True)
+        print('SALE | {}'.format(sales_json_list))
         date_json = getDateJson(event)
         print('DATE | {}'.format(date_json))
-        pretty_sales = '\n'.join([pretty(x, 1) for x in sales_json]).decode('utf-8')
-        event_header = "--".join([event['name'], event['url']])
-        presale_info_string = "\n===\n".join([event_header, pretty_sales])
-        full_presale_info_string += '\n============\n' + presale_info_string.encode('utf-8')
-    '''
     address_list = ['brandonjflannery@gmail.com', 'jaredbaker5@gmail.com']
     emailPresaleInfo(event_list, address_list)    
 
