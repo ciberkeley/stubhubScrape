d6d7e798b7a02d261688bf6cfbd99f6e7d52f130
root <root@brandon-1.c.resell-157718.internal>
2017-02-19 19:38:49 +0000

    Adding dependency isntall script

---
+++ src/dependencyInstall.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get upgrade python-dev gcc tor
+apt-get install python-torctl gcc tor
+easy_install lxml
+
+pip install pandas bs4 html google.cloud numpy pymongo html2text lxml boto pyjarowinkler pysocks stem --upgrade
+
